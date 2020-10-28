5f3f3c79c5bae94f4f5c8a49d0a752d65075d6b2
ValentinGor <v.gorbatenko@mail.ru>
2020-10-28 21:06:45 +0200

    Интеграция первой страницы

--- wp-content/themes/blueresume/inc/carbon.php
+++ wp-content/themes/blueresume/inc/carbon.php
@@ -6,6 +6,21 @@ add_action( 'carbon_fields_register_fields', 'crb_attach_theme_options' );
 function crb_attach_theme_options() {
     Container::make( 'theme_options', __( 'Theme Options' ) )
         ->add_fields( array(
-            Field::make( 'text', 'crb_text', 'Text Field' ),
+            Field::make( 'image', 'logo_image', __( 'Logo' ) )
+                ->set_value_type( 'url' ),
+            Field::make( 'complex', 'social_links', __( 'Social Links' ) )
+                ->add_fields( array(
+                    Field::make( 'text', 'fonts_code', __( 'Код шрифта' ) ),
+                    Field::make( 'text', 'social_link', __( 'Ссылка на сеть' ) ),
+                ) ),
+            Field::make( 'textarea', 'footer_text', __( 'Текст в футере' ) ),
+        ) );
+
+    Container::make( 'post_meta', 'main-page', __( 'Содержимое Прайс', 'blueresume' ) )
+        ->where( 'post_type', '=', 'page' )
+        ->where( 'post_template', '=', 'main-page.php' )
+        ->add_fields( array(
+            Field::make( 'text', 'main-slag', __( 'Заголовок страницы', 'blueresume' ) )
+                ->set_required( true ),
         ) );
 }
---
+++ wp-content/themes/blueresume/main-page.php
@@ -0,0 +1,160 @@
+<?php
+/*
+Template Name: Main Page
+*/
+?>
+<?php get_header()?>
+<div class="line">
+    <header>
+        <div class="top__line">
+            <div class="top__line_left">
+                <div class="top__line_left-menu">
+                    <ul>
+                        <li><a class="scroll-to" href="#Works">Works</a></li>
+                        <li><a class="scroll-to" href="#About">About</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="top__line_center">
+                <?php if ( carbon_get_theme_option( 'logo_image' ) ) { ?>
+                    <div class="top__line_center-logo">
+                        <a href="<?php get_home_url();?>"><img src="<?php echo carbon_get_theme_option( 'logo_image' ); ?>" alt="img"></a>
+                    </div>
+                <?php } ?>
+            </div>
+            <div class="top__line_right">
+                <div class="top__line_right-menu">
+                    <ul>
+                        <li><a class="scroll-to" href="#Links">Links</a></li>
+                        <li><a class="scroll-to" href="#Contacts">Contacts</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div id="button_mobile__menu" class="mobile__menu"></div>
+    <div id="block_mobile__menu" class="mobile__block">
+        <ul>
+            <li><a class="scroll-to" href="#Works">Works</a></li>
+            <li><a class="scroll-to" href="#About">About</a></li>
+            <li><a class="scroll-to" href="#Links">Links</a></li>
+            <li><a class="scroll-to" href="#Contacts">Contacts</a></li>
+        </ul>
+    </div>
+    <section>
+
+        <?php if ( carbon_get_post_meta( get_the_ID(), 'main-slag' ) ) { ?>
+            <div class="main__slogan">
+                <div class="main__slogan_head">
+                    <h1><?php echo carbon_get_post_meta( get_the_ID(), 'main-slag' ); ?></h1>
+                </div>
+            </div>
+        <?php } ?>
+
+
+        <div class="social__line">
+            <?php $data_social_links = carbon_get_theme_option( 'social_links' ); ?>
+            <?php if ( ! empty( $data_social_links ) ): ?>
+                <?php foreach ( $data_social_links as $item_link ): ?>
+                    <a href="<?php echo $item_link['social_link'] ?>" target="_blank"><span class="icon <?php echo $item_link['fonts_code'] ?>"></span></a>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </div>
+    </section>
+
+    <section>
+        <div id="Works"></div>
+        <div class="works">
+            <div class="works__head">
+                <h2>Works</h2>
+            </div>
+            <div class="works__left">
+                <div class="project" style="background-image: url(<?php echo get_template_directory_uri();?>/assets/img/img_1.webp);">
+                    <div class="project__links">
+                        <a href="#" target="_blank">Featured</a>
+                    </div>
+                    <div class="project__text">
+                        Motivation Is The First Step To Success
+                    </div>
+                </div>
+                <div class="project" style="background-image: url(<?php echo get_template_directory_uri();?>/assets/img/img_2.webp);">
+                    <div class="project__text">
+                        Benjamin Franklin S Method Of Habit Formation
+                    </div>
+                </div>
+            </div>
+            <div class="works__right">
+                <div class="project" style="background-image: url(<?php echo get_template_directory_uri();?>/assets/img/img_3.webp);">
+                    <div class="project__text">
+                        How Great Is The Strength Of Your Belief
+                    </div>
+                </div>
+                <div class="project" style="background-image: url(<?php echo get_template_directory_uri();?>/assets/img/img_4.webp);">
+                    <div class="project__links">
+                        <a href="#" target="_blank">Featured</a>
+                    </div>
+                    <div class="project__text">
+                        Increasing Prosperity With Positive Thinking
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div id="About"></div>
+        <div class="about">
+            <div class="about__head">
+                <h2>About</h2>
+            </div>
+            <div class="about__info">
+                <div class="about__info_head">
+                    <h3>UX/UI</h3>
+                </div>
+                <div class="about__info_text myClass">
+                    Of course, motivation really isn’t <a href="#" target="_blank">simple at all</a>. That is why there is multi-billion industry focused on self-help books, tapes, seminars, camps, and coaches.
+                </div>
+            </div>
+            <div class="about__info">
+                <div class="about__info_head">
+                    <h3 id="Motion">Motion</h3>
+                </div>
+                <div class="about__info_text myClass">
+                    As humans, we have progressed in so many ways, but we have lost touch with our bodies, nature, and the power of prayer. There seems to be a spiritual renaissance, of sorts, going on in the
+                    “new age” movement.
+                </div>
+            </div>
+            <div class="about__info">
+                <div class="about__info_head">
+                    <h3>Branding</h3>
+                </div>
+                <div class="about__info_text myClass">
+                    If at anytime, you notice that a friend of yours is getting demotivated about life, try to inspire him/her as much as you can. Even if you are physically away from your friend you can send
+                    him inspirational ecards and tell him to download motivational screensavers and wallpapers. So motivate everyone who needs the boost.
+                </div>
+            </div>
+            <div class="about__info">
+                <div class="about__info_head">
+                    <h3>Development</h3>
+                </div>
+                <div class="about__info_text myClass">
+                    Personally, the importance does not lie in what religion you are; however, a lack of religion does seem to run parallel with depression. This is classified as spiritual illness, which will
+                    have a negative effect on you mentally, physically, or totally.
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div id="Links"></div>
+        <div class="links">
+            <div class="links__head">
+                <h2>Links</h2>
+            </div>
+        </div>
+    </section>
+    <span class="line-1"></span>
+    <span class="line-2"></span>
+    <span class="line-3"></span>
+    <span class="line-4"></span>
+    <span class="line-5"></span>
+</div>
+<?php get_footer()?>
